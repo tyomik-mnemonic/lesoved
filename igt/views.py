dd9b6e1e9261618c7ed9b27af84f76cc9a0df2a0
tyomic-mnemonic <vins90nuar@gmail.com>
2019-02-13 12:19:25 +0300

    whole models.py was connected with exist DB. Test API, and test Admin sets were created.

--- igt/views.py
+++ igt/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from django.shortcuts import render
+from rest_framework import viewsets, generics, permissions, filters, decorators
+
+from .models import *
+from .serializers import *
+
+
+class AddressesViewSet(viewsets.ModelViewSet):
+    queryset = Addresses.objects.all()
+    serializer_class = AddressesSerializer
+    permission_classes = (permissions.AllowAny,)
