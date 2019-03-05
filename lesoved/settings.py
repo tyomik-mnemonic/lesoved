2ea0b065d0e6b920e70a61c1052ce8491ef9bd80
slavas <slavas@bk.ru>
2019-03-05 12:27:22 +0300

    Изменяем локальный доступ к БД

--- lesoved/settings.py
+++ lesoved/settings.py
@@ -91,25 +91,19 @@ WSGI_APPLICATION = 'lesoved.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-
 DATABASES = {
-    'default': dj_database_url.config(default='postgres://'),
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'lesoved',
+        'USER': 'postgres',
+        'HOST': '172.17.0.2',
+        'PASSWORD': 'ololopostgres88',
+        'PORT': '5432',
+        'OPTIONS': {
+            'options': '-c search_path=forest'
+         }
+    }
 }
-
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-#        'NAME': 'lesoved',
-#        'USER': 'postgres',
-#        'HOST': '172.16.16.40',
-#        'PASSWORD': 'igt_3631',
-#        'PORT': '25432',
-#        'OPTIONS': {
-#            'options': '-c search_path=forest'
-#         }
-#    }
-# }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
