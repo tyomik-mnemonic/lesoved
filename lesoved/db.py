647a63222d3bdd2a9d9780903e4fdf4da2f31ab5
slavas <slavas@bk.ru>
2019-03-06 10:55:01 +0300

    Вариант с файлом db

--- lesoved/db.py
+++ lesoved/db.py
@@ -1,8 +1,22 @@
-import dj_database_url
+# import dj_database_url
 
 
-DATABASE_ENGINE_POSTGRESQL = 'django.contrib.gis.db.backends.postgis'
+# DATABASE_ENGINE_POSTGRESQL = 'django.contrib.gis.db.backends.postgis'
+
+# DATABASES = {
+#     'default': dj_database_url.config(default='postgres:///'),
+# }
 
 DATABASES = {
-    'default': dj_database_url.config(default='postgres:///'),
+   'default': {
+       'ENGINE': 'django.contrib.gis.db.backends.postgis',
+       'NAME': 'lesoved',
+       'USER': 'postgres',
+       'HOST': '172.17.0.2',
+       'PASSWORD': 'igt_3631',
+       'PORT': '5432',
+       'OPTIONS': {
+           'options': '-c search_path=forest'
+        }
+    }
 }
