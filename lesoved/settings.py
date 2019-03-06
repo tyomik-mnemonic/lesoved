6338d6571a7ade439550f940d0dc9eda2d0531b3
slavas <slavas@bk.ru>
2019-03-06 15:54:19 +0300

    Разделяем БД на тестовую и продакшн

--- lesoved/settings.py
+++ lesoved/settings.py
@@ -89,6 +89,13 @@ WSGI_APPLICATION = 'lesoved.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+from .db import *
+
+try:
+    from .settings_local import *
+except ImportError:
+    pass
+
 # DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.contrib.gis.db.backends.postgis',
@@ -159,11 +166,3 @@ STATICFILES_DIRS = (
 STATIC_ROOT = os.path.join(ENV_DIR, 'www', 'static')
 
 STATIC_URL = '/static/'
-
-from .db import *
-
-try:
-    from settings_local import *
-except ImportError:
-    pass
-
